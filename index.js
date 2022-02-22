1b6145bed910415e688610502411450eb5ce5808
David Stockdale <dstock3@gmail.com>
2022-02-22 13:44:10 -0600

    Created the initial setup for first Node.js project.

---
+++ index.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const path = require('path')
+const html = require('html')
+
+const PORT = process.env.PORT || 5500
+
+const server = http.createServer((req, res) => {
+
+}).listen(PORT, () => console.log(`The Server is currently running on ${PORT}`))
+
+
+

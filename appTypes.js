60c6ffc7c074fc1a07fae591b8f733fbb1d2e778
David Stockdale <dstock3@gmail.com>
2022-02-22 14:37:07 -0600

    Fixed error in require statement. Set watch and build scripts in package configuration. Created separate file for the appTypes object which is then required in index via common js.

---
+++ appTypes.js
@@ -0,0 +1,12 @@
+// Account for any file extensions you might come across
+
+const appTypes = [
+    {".js": "text/javascript"},
+    {".css": "text/css"},
+    {".json": "application/json"},
+    {".png": "image/png"},
+    {".jpg": "image/jpg"},
+    {".svg": "image/svg"}
+]
+
+module.exports = appTypes;
--- index.js
+++ index.js
@@ -1,36 +1,26 @@
 const fs = require('fs');
-const path = require('path')
-const html = require('html')
+const path = require('path');
+const http = require('http');
+const appTypes = require('./appTypes.js')
 
-const PORT = process.env.PORT || 5500
+const PORT = process.env.PORT || 8080
 
 const distFolder = "public"
 const htmlType = "text/html"
 
-const server = http.createServer((req, res) => {
+http.createServer((req, res) => {
     // Build the file path
     let filePath = path.join(__dirname, distFolder, req.url === '/' ? 'index.html' : req.url)
 
     // Get the file extension
     let extname = path.extname(filePath)
 
-    // Account for any file extensions you might come across
-    const appTypes = [
-        {".js": "text/javascript"},
-        {".css": "text/css"},
-        {".json": "application/json"},
-        {".png": "image/png"},
-        {".jpg": "image/jpg"},
-        {".svg": "image/svg"}
-    ]
-
     // By default, we're looking for text/html files
     let contentType = htmlType
 
-    // But if the extension is something else, the contentType is assigned accordingly...
+    // But if the extension is something else, the contentType is assigned accordingly based on data in appTypes...
     for (let prop in appTypes) {
         if (extname === prop) {
-            check = true
             contentType = appTypes[prop]
         }
     }
@@ -42,21 +32,22 @@ const server = http.createServer((req, res) => {
             //Account for Page Not Found
             if (err.code === "ENOENT") {
                 fs.readFile(
-                    path.join(__dirname, distFolder, "404.html", (err, content) => {
+                    path.join(__dirname, distFolder, "404.html"), (err, content) => {
                         res.writeHead(404, {"Content-Type": htmlType})
                         res.end(content, "utf8")
-                    })
-                )
+                    }) 
             } else {
                 //Account for other server errors
                 res.writeHead(500);
-                res.end(content, "utf8")
+                res.end(`Server Error: ${err.code}`)
             }
-
+        } else {
+            //If there are no errors, delivery the content
+            res.writeHead(200, {"Content-Type": contentType});
+            res.end(content, "utf8")
         }
     })
-
-}).listen(PORT, () => console.log(`The Server is currently running on ${PORT}`))
+}).listen(PORT, () => console.log(`The Server is currently running on ${PORT}.`))
 
 
 

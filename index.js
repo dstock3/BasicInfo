1fa8933b17be59bf4a325ad3c95f99086bdb8e2e
David Stockdale <dstock3@gmail.com>
2022-02-22 14:13:31 -0600

    Called the readFile method from the fs module to account for server errors and page not found.

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ const html = require('html')
 
 const PORT = process.env.PORT || 5500
 
+const distFolder = "public"
+const htmlType = "text/html"
+
 const server = http.createServer((req, res) => {
     // Build the file path
-    let filePath = path.join(__dirname, "public", req.url === '/' ? 'index.html' : req.url)
+    let filePath = path.join(__dirname, distFolder, req.url === '/' ? 'index.html' : req.url)
 
     // Get the file extension
-    let extname = path.extname(filePat)
-
-    // By default, we're looking for text/html files
-    let contentType = "text/html"
+    let extname = path.extname(filePath)
 
     // Account for any file extensions you might come across
     const appTypes = [
@@ -20,12 +20,17 @@ const server = http.createServer((req, res) => {
         {".css": "text/css"},
         {".json": "application/json"},
         {".png": "image/png"},
-        {".jpg": "image/jpg"}
+        {".jpg": "image/jpg"},
         {".svg": "image/svg"}
     ]
 
+    // By default, we're looking for text/html files
+    let contentType = htmlType
+
+    // But if the extension is something else, the contentType is assigned accordingly...
     for (let prop in appTypes) {
         if (extname === prop) {
+            check = true
             contentType = appTypes[prop]
         }
     }
@@ -35,6 +40,18 @@ const server = http.createServer((req, res) => {
     fs.readFile(filePath, (err, content) => {
         if (err) {
             //Account for Page Not Found
+            if (err.code === "ENOENT") {
+                fs.readFile(
+                    path.join(__dirname, distFolder, "404.html", (err, content) => {
+                        res.writeHead(404, {"Content-Type": htmlType})
+                        res.end(content, "utf8")
+                    })
+                )
+            } else {
+                //Account for other server errors
+                res.writeHead(500);
+                res.end(content, "utf8")
+            }
 
         }
     })
